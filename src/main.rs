26781e3cba9da1d7d7d064e832dc78b084d4d693
Nick <nick@Nicks-MacBook-Pro.local>
2025-07-11 20:37:30 +0300

    added user-agent and x-client-id header processing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::path::Path;
+
 use axum::{
     Router,
     body::Bytes,
     extract::Multipart,
+    http::HeaderMap,
     routing::{get, post},
 };
-use tokio::{fs::File, io::AsyncWriteExt};
+use clap::crate_version;
+use tokio::{
+    fs::{self, File},
+    io::AsyncWriteExt,
+};
 
 #[tokio::main]
 async fn main() {
@@ -15,24 +22,49 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn index() -> &'static str {
-    "🐙 Welcome to OktoSync!"
+async fn index() -> String {
+    format!("🐙 Welcome to OktoSync {}!", crate_version!())
 }
 
-async fn upload(mut multipart: Multipart) {
+async fn upload(headers: HeaderMap, mut multipart: Multipart) {
+    let user_agent = get_header(&headers, "user-agent");
+    let client_id = get_header(&headers, "x-client-id");
+
+    println!("UserAgent: {user_agent}");
+    println!("ClientID: {client_id}");
+
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.file_name().unwrap().to_string();
         let data = field.bytes().await.unwrap();
 
+        let path = format!("uploads/{client_id}/{name}"); // TODO: config
+
         println!("Length of `{name}` is {} bytes.", data.len());
-        if let Err(e) = save_file(&name, data).await {
+        if let Err(e) = save_file(&path, data).await {
             eprintln!("Failed to save file: {e}");
         };
     }
 }
 
-async fn save_file(file_name: &str, data: Bytes) -> std::io::Result<()> {
-    let mut file = File::create(file_name).await?;
+async fn register_client() {}
+
+async fn save_file(path: &str, data: Bytes) -> std::io::Result<()> {
+    let path = Path::new(path);
+
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent).await?;
+    }
+
+    let mut file = File::create(path).await?;
+
     file.write_all(&data).await?;
     Ok(())
+}
+
+fn get_header(headers: &HeaderMap, name: &str) -> String {
+    headers
+        .get(name)
+        .and_then(|h| h.to_str().ok())
+        .unwrap_or("unknown")
+        .to_string()
 }
